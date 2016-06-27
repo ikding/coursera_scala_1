e3d0ce3a56e39920164e558c8aac4af7558c3312
I-Kang Ding <ikang.ding@capitalone.com>
2016-06-27 13:37:44 -0400

    Add solution to week 1 assignment

--- assignments/recfun/src/main/scala/recfun/Main.scala
+++ assignments/recfun/src/main/scala/recfun/Main.scala
@@ -1,27 +1,50 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
-  def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
+    def main(args: Array[String]) {
+        println("Pascal's Triangle")
+        for (row <- 0 to 10) {
+            for (col <- 0 to row)
+                print(pascal(col, row) + " ")
+            println()
+        }
     }
-  }
 
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+    def pascal(c: Int, r: Int): Int = {
+        if (c == 0 || c == r) 1
+        else pascal(c-1, r-1) + pascal(c, r-1)
+  }
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    @tailrec
+    def balance(chars: List[Char], buffer: Int = 0): Boolean = {
+        if (chars.isEmpty || buffer < 0)
+            buffer == 0
+        else if (chars.head == '(')
+            balance(chars.tail, buffer + 1)
+        else if (chars.head == ')')
+            balance(chars.tail, buffer - 1)
+        else
+            balance(chars.tail, buffer)
+  }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if ((money < 0) || coins.isEmpty) 0
+      else {
+          if (money == 0) 1
+          else {
+              countChange(money - coins.head, coins) + countChange(money, coins.tail)
+          }
+      }
+  }
   }
